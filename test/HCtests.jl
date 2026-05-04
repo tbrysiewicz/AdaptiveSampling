ae404d073d9ce264206e8c773ab40f92d528c88a
tbrysiewicz <taylorbrysiewicz@gmail.com>
2026-05-04 13:36:47 -0400

    Add visualize_dietmaier function for general system visualization

--- test/HCtests.jl
+++ test/HCtests.jl
@@ -22,3 +22,16 @@ function visualize_real_solutions(F::System)
     visualize(VSD)
     return(VSD)
 end
+#Do the above code for a general system of equations
+function visualize_dietmaier(F::System)
+    #Check if F has 2 parameters
+    if length(parameters(F)) != 2
+        throw(ArgumentError("The system must have exactly 2 parameters for visualization."))
+    end
+    @var x,y
+    f(p1,p2) = solve(F;target_parameters=[p1,p2])
+    VSD = ValuedSubdivision(f; xlims=[-10, 10], ylims=[-10, 10], resolution=20)
+    refine!(VSD, 500) 
+    visualize(VSD)
+    return(VSD)
+end
